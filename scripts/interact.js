47b78ae1f49761c2181079c11098e803e7b06d33
Vincent Nguyen <vtn28@drexel.edu>
2018-03-15 16:56:17 -0400

    v.0.1.1
    More RNG, some text updates/fixes.

--- scripts/interact.js
+++ scripts/interact.js
@@ -22,14 +22,6 @@ var playerHealth = MAXHEALTH
 var inBattle = false
 var inProgress = false
 
-// Resets player to (0,0)
-function setSpawn(x, y) {
-    playerPosX = x
-    playerPosY = y
-    playerPos = '(' + playerPosX + ',' + playerPosY + ')'
-    // console.log("Spawn set to" + playerPos)
-}
-
 function startGame() {
     if(inProgress) {
         if(confirm("Restart the game?")) {
@@ -46,12 +38,22 @@ function startGame() {
         setBoard()
     }
     inProgress = true
+    document.getElementById("board").style.opacity = 1;
 }
 
 function saveGame() {
-    console.log("Saving...")
+    console.log("WARNING: Not Implemented")
+    //console.log("Saving...")
     // Run save
-    console.log("Game saved.")
+    //console.log("Game saved.")
+}
+
+// Sets player position to (x,y)
+function setSpawn(x, y) {
+    playerPosX = x
+    playerPosY = y
+    playerPos = '(' + playerPosX + ',' + playerPosY + ')'
+    // console.log("Spawn set to" + playerPos)
 }
 
 // Sets width and height of board, then generates it
@@ -68,6 +70,7 @@ function setBoard() {
 
     document.getElementById("level").innerHTML = level
     document.getElementById("playerHealth").innerHTML = playerHealth
+    document.getElementById("gold").innerHTML = gold
     makeBoard()
 }
 
@@ -76,7 +79,7 @@ function makeBoard() {
     var board = document.getElementById("board")
     var str = ''
     if(boardWidth < 1 || boardHeight < 1) {
-        console.log("Cannot create board.")
+        console.log("Board too small.")
         return
     }
     for(var j = 0; j < boardHeight; j++) {
@@ -151,103 +154,14 @@ function setExit() {
     // console.log("exit at: " + exitPos)
 }
 
-// Play event when player reaches certain coordinate
-function playEvent() {
-    console.log("Event!")
-    gold += 1
-    var bonus = Math.floor(Math.random())
-    playerHealth += bonus
-    if(bonus > 0)
-        document.getElementById("eventDesc").innerHTML = 'You found a health potion among the treasure. Gold +1, Health +1'
-    else
-        document.getElementById("eventDesc").innerHTML = 'You found some gold pieces. Gold +1'
-    document.getElementById("gold").innerHTML = gold
-}
-
-function playBattle() {
-    console.log("Battle!")
-    var modal = document.getElementById("battleModal")
-    showBattle(modal)
-    var atkBtn = document.getElementById("atk")
-
-    inBattle = true
-    monsterHealth = Math.floor(Math.random() * 4 + 1)
-    document.getElementById("eventDesc").innerHTML = 'You fought a monster and took ' + monsterHealth + ' damage.'
-
-    // document.getElementById("playerHealth").innerHTML = playerHealth
-    document.getElementById("playerHP").innerHTML = playerHealth
-    document.getElementById("monsterHP").innerHTML = monsterHealth
-
-    console.log("Entering battle")
-    atkBtn.onclick = function() {
-        console.log("ATTACK")
-        monsterHealth -= 1
-        playerHealth -= 1
-        document.getElementById("playerHP").innerHTML = playerHealth
-        document.getElementById("monsterHP").innerHTML = monsterHealth
-        if(monsterHealth <= 0) {
-            closeBattle(modal)
-            inBattle = false
-            document.getElementById("playerHealth").innerHTML = playerHealth
-        }
-        if(playerHealth <= 0) {
-            closeBattle(modal)
-            document.getElementById("playerHealth").innerHTML = playerHealth
-            defeat()
-        }
-    }
-    console.log("Battle finished")
-}
-
-function showBattle(modal) {
-    modal.style.display = "block"
-    document.getElementById("atk").focus();
-}
-
-function closeBattle(modal) {
-    modal.style.display = "none"
-}
-
-function playLucky() {
-    console.log("Lucky!")
-    playerHealth += 10
-    if(playerHealth > MAXHEALTH)
-        playerHealth = MAXHEALTH
-    document.getElementById("playerHealth").innerHTML = playerHealth
-    document.getElementById("eventDesc").innerHTML = 'You find a large health potion. Health +10'
-}
-
-function exit() {
-    if(level == 10) {
-        finish()
-        return
-    }
-    if(confirm("You've reached the exit. Proceed to next level?")) {
-        level += 1
-        setBoard()
-    }
-    else {
-        console.log('exit canceled')
-    }
-    return
-}
-
-function finish() {
-    alert("You reached the end!")
-    inProgress = false
-}
-
-function defeat() {
-    alert("Game over. You collected " + gold + " treasures.")
-    inProgress = false
-}
-
 // Marks tiles as visited as the player reaches them
 function visit() {
     var tile = document.getElementById(playerPos)
     if(tile.className == "undiscovered") {
         tile.className = "discovered"
         document.getElementById("eventDesc").innerHTML = ''
+        document.getElementById("gold").innerHTML = gold
+        document.getElementById("playerHealth").innerHTML = playerHealth
     }
     if(tile.className == "hidevent") {
         tile.className = "foundevent"
@@ -271,7 +185,6 @@ function visit() {
     if(tile.className == "exit") {
         exit()
     }
-    document.getElementById("playerHealth").innerHTML = playerHealth
 }
 
 // Moves the player up, down, left, or right
@@ -310,4 +223,169 @@ function movePlayer(e) {
     setPlayer()
     visit()
     // console.log("Moved to (" + playerPosX + "," + playerPosY + ")")
+}
+
+// EVENTS **** 
+
+// Play event when player reaches certain coordinate
+function playEvent() {
+    console.log("Event!")
+    gold += 1
+    var bonus = Math.floor(Math.random() * 4)
+    if(bonus < 1) {
+        playerHealth += 1
+        if(playerHealth > MAXHEALTH)
+            playerHealth = MAXHEALTH
+        document.getElementById("playerHealth").innerHTML = playerHealth + "  (<b style='color: green'>+1</b>)"
+        document.getElementById("eventDesc").innerHTML = 'You found a health potion among the treasure. Gold +1, Health +1'
+    }
+    else
+        document.getElementById("eventDesc").innerHTML = 'You found some gold pieces. Gold +1'
+    document.getElementById("gold").innerHTML = gold + "  (<b style='color: rgb(150, 150, 0)'> +1 </b>)"
+}
+
+function playBattle() {
+    console.log("Battle!")
+    var modal = document.getElementById("battleModal")
+    showBattle(modal)
+    var atkBtn = document.getElementById("atk")
+    var fleeBtn = document.getElementById("flee")
+    var doneBtn = document.getElementById("leaveBattle")
+    inBattle = true
+    monsterHealth = Math.floor(Math.random() * 4 + 1)
+    var startHP = playerHealth
+    var battleTxt = document.getElementById("battleText")
+
+    // document.getElementById("playerHealth").innerHTML = playerHealth
+    document.getElementById("playerHP").innerHTML = playerHealth
+    document.getElementById("monsterHP").innerHTML = monsterHealth
+
+    console.log("Entering battle")
+
+    battleTxt.innerHTML = ""
+
+    atkBtn.onclick = function() {
+        var mRoll = Math.floor(Math.random() * 10)
+        var pRoll = Math.floor(Math.random() * 10)
+
+        if(mRoll == 0)
+            mAtk = mRoll
+        else {
+            mAtk = 1
+        }
+        if(pRoll == 0)
+            pAtk = pRoll
+        else {
+            pAtk = 1
+        }
+
+        monsterHealth -= pAtk
+        playerHealth -= mAtk
+        document.getElementById("playerHP").innerHTML = playerHealth + "   (<b style='color: red'>-" + mAtk + "</b>)"
+        document.getElementById("monsterHP").innerHTML = monsterHealth + "   (<b style='color: red'>-" + pAtk + "</b>)"
+
+        if(mAtk == 0) {
+            if(pAtk == 0)
+                battleTxt.innerHTML = "You and the monster whiffed your attacks."
+            if(pAtk == 1)
+                battleTxt.innerHTML = "You dodge the monster's attack and deal 1 damage."
+        }
+        if(mAtk == 1) {
+            if(pAtk == 0)
+                battleTxt.innerHTML = "You miss, and the monster deals 1 damage to you."
+            if(pAtk == 1)
+                battleTxt.innerHTML = "You and the monster take 1 damage."
+        }
+
+        var hpDiff = startHP - playerHealth
+        document.getElementById("eventDesc").innerHTML = 'You fought a monster and took ' + hpDiff + ' damage.'
+
+        if(playerHealth <= 0) {
+            // closeBattle(modal)
+            atkBtn.style.display = "none"
+            fleeBtn.style.display = "none"
+            doneBtn.style.display = "inline"
+            document.getElementById("playerHealth").innerHTML = playerHealth
+            battleTxt.innerHTML += " You are defeated."
+            document.getElementById("eventDesc").innerHTML += '<br>Game Over.'
+            document.getElementById("leaveBattle").focus()
+            defeat()
+        }
+        else if(monsterHealth <= 0) {
+            atkBtn.style.display = "none"
+            fleeBtn.style.display = "none"
+            doneBtn.style.display = "inline"
+            battleTxt.innerHTML += " The monster is slain!"
+            document.getElementById("leaveBattle").focus()
+        }
+    }
+
+    fleeBtn.onclick = function() {
+        if(gold <= 4)
+            battleTxt = "Cannot flee!"
+        else {
+            diff = Math.floor(gold * (1/4))
+            gold -= diff
+            document.getElementById("gold").innerHTML = gold + "  (<b style='color: red'>-" + diff + "</b>)"
+            document.getElementById("eventDesc").innerHTML = "You drop some of your gold as you flee."
+            closeBattle(modal)
+            inBattle = false
+        }
+    }
+    doneBtn.onclick = function() {
+        closeBattle(modal)
+        if(playerHealth > 0)
+            inBattle = false
+        document.getElementById("playerHealth").innerHTML = playerHealth
+        atkBtn.style.display = "inline"
+        fleeBtn.style.display = "inline"
+        doneBtn.style.display = "none"
+    }
+    console.log("Battle finished")
+}
+
+function showBattle(modal) {
+    modal.style.display = "block"
+    document.getElementById("atk").focus()
+}
+
+function closeBattle(modal) {
+    modal.style.display = "none"
+}
+
+function playLucky() {
+    console.log("Lucky!")
+    playerHealth += 5
+    if(playerHealth > MAXHEALTH)
+        playerHealth = MAXHEALTH
+    document.getElementById("playerHealth").innerHTML = playerHealth + " (<b style='color: green'>+5</b>)"
+    document.getElementById("eventDesc").innerHTML = 'You find a large health potion. Health +5'
+}
+
+function exit() {
+    if(level == 10) {
+        finish()
+        return
+    }
+    if(confirm("You've reached the exit. Proceed to next level?")) {
+        level += 1
+        setBoard()
+    }
+    else {
+        console.log('exit canceled')
+    }
+    return
+}
+
+// END GAME ****
+
+function finish() {
+    alert("You reached the end! Treasures collected: " + gold)
+    inProgress = false
+}
+
+function defeat() {
+    alert("Game over. You collected " + gold + " treasures.")
+    document.getElementById("board").style.opacity = 0.5;
+    inProgress = false
 }
